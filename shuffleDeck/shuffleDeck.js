edb1284e907857a0f577aeaac333f4fe4baf2c77
Doug Perez <perezcpt@gmail.com>
2020-12-12 20:51:42 -0800

    test

--- shuffleDeck/shuffleDeck.js
+++ shuffleDeck/shuffleDeck.js
@@ -1,36 +1,3 @@
-/**
- * Given an array containing a deck of cards, implement a function that shuffles
- * the deck.
- *
- * Example:
- *  var deck = orderedDeck();
- *  // ["A♥","2♥","3♥",...,"J♦","Q♦","K♦"]
- *  shuffleDeck(deck);
- *  // ["2♠","J♣","A♦", ... ,"7♣","8♣","K♠"]
- *
- * Note:
- *   A shuffled deck should be completely random. That means that a given card should
- *   be as likely as any other to appear in a given deck index, completely independent
- *   of the order of the input deck. Think carefully about how to be sure your algorithm
- *   generates a properly shuffled deck-- it is easy to accidentally create a biased algorithm.
- *
- * Extra credit:
- *   - Even a naive algorithm can easily run in linear time. However, does your
- *     algorithm remain unbiased as N (the deck size) increases? How do you know?
- *   - Once you have created a properly random, linear algorithm, what is its space complexity?
- *     There is an algorithm that uses O(N) time and O(1) space (i.e., an in-place shuffle).
- *
- * A further note on randomness:
- *   Technically, a computer-shuffled deck will usually be "pseudorandom",
- *   not "truly" random. However, the difference between the two is too small to
- *   be detectable by any known test.
- *   See http://en.wikipedia.org/wiki/Pseudorandom_number_generator .
- *
- *   A human shuffler is much more biased; it takes around seven normal "riffle"
- *   shuffles before a real deck is actually randomized.
- *   See https://www.dartmouth.edu/~chance/teaching_aids/books_articles/Mann.pdf .
- */
-
 
 
 //i: sorted array
@@ -58,23 +25,36 @@ var orderedDeck = function () {
   return deck;
 };
 
+
+// console.log('test', test)
+
 var sorted = orderedDeck()
 
 console.log('sorted', sorted)
-
 var shuffleDeck = function (deck) {
-  for (var i = deck.length - 1; i > 0; i--) {
-    console.log(i)
-    var k = Math.random() * (52 - 1) + 1;
+  var i = deck.length
+  var j = 0;
+  let temp;
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
 
-
-    var current = deck[i];
-    deck[i] = deck[k]
-    deck[k] = deck;
+    temp = deck[i];
+    deck[i] = deck[j]
+    deck[j] = temp;
   }
-
-  return deck;
+  return deck
 };
 
 console.log('Result', shuffleDeck(sorted))
+
+
+//define var for deck.length j = 0, and a temp
+//while subtracting 1 from array.length each time.
+    //assign j to a random number function
+    //temp is set to array[i]
+
+
+
+
+
 
