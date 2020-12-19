b55eb962c4796512342d56d942c3b72a554b3842
Doug Perez <perezcpt@gmail.com>
2020-12-18 19:57:47 -0800

    powerset

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,25 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+var allAnagrams = function (string) {
+  var anagram = {};
+  var generator = function (text, options) {
+    if (text.length === string.length) {
+      anagram[text] = true;
+    }
+    for (var i = 0; i < options.length; i++) {
+      generator(text + options[i],
+        options.slice(0, i) + options.slice(i + 1))
+      console.log('options', options)
+    }
+  }
+  generator('', string);
+  return Object.keys(anagram);
 };
+
+
+var test1 = allAnagrams('abc')
+console.log(test1) //[ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+
+var test2 = allAnagrams('ab')
+console.log(test2) //[ 'ab', 'ba']
--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -4,18 +4,42 @@
  *
  * Example:
  *
- * powerSet("abc")
- * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
+
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str) {
+var powerSet = function (str) {
+  if (str.length === 1) {
+  }
+  var obj = {}
+  for (var i = 0; i < str.length; i++) {
+    var end = str[i + 1]
+    if (str[i + 1] === undefined) {
+      end = str[0]
+    }
+    console.log(str[i] + end)
+
+
+  }
 };
+
+console.log(powerSet("abc"))
+//[ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
+
+console.log(powerSet("jump"))
+
+
+//i: string
+//o: array of strings
+//e: ba and ab are considered duplicates and only count once. Also count the empty string
+
+
+//if string.length === 1,
