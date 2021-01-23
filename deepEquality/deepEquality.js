c661335d1d445ee6ee91a58e48437d6d6f5bbccf
Doug Perez <perezcpt@gmail.com>
2021-01-22 19:46:33 -0800

    Deep Equality completed

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,23 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
+
+
+
+
+var deepEquals = function (apple, orange) {
+  var StringApple = JSON.stringify(apple);
+  var StringOrange = JSON.stringify(orange);
+  return (StringApple === StringOrange);
 };
+
+var exampleTrue = deepEquals({ a: 1, b: { c: 3 } }, { a: 1, b: { c: 3 } }); // true
+
+var exampleFalse = deepEquals({ a: 1, b: { c: 5 } }, { a: 1, b: { c: 6 } }); // false
+
+console.log(exampleFalse)
+
+//i: two objects
+//o: boolean
+//c: n/a
+//e: n/a
