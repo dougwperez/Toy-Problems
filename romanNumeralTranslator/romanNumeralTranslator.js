3104f659e1af1b480d4ca0beb9bbf4f738240ee8
Doug Perez <perezcpt@gmail.com>
2021-01-25 20:39:47 -0800

    Roman Numerals

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -1,22 +1,3 @@
-
-/**
- * Given a roman numeral as input, write a function that converts the roman
- * numeral to a number and outputs it.
- *
- * Ex:
- * translateRomanNumeral("LX") // 60
- *
- * When a smaller numeral appears before a larger one, it becomes
- * a subtractive operation. You can assume only one smaller numeral
- * may appear in front of larger one.
- *
- * Ex:
- * translateRomanNumeral("IV") // 4
- *
- * You should return `null` if the input is not a string. You can expect
- * all non-empty string inputs to be valid roman numerals.
- */
-
 var DIGIT_VALUES = {
   I: 1,
   V: 5,
@@ -27,7 +8,32 @@ var DIGIT_VALUES = {
   M: 1000
 };
 
-var translateRomanNumeral = function(romanNumeral) {
-// TODO: Implement me!
+var translateRomanNumeral = function (romanNumeral) {
+  if (typeof romanNumeral !== 'string') {
+    return null;
+  } else {
+    var split = romanNumeral.split('');
 
-};
+    var newNums = [];
+    for (var i = 0; i < split.length; i++) {
+      var currentNumeral = split[i]
+      for (var key in DIGIT_VALUES) {
+        if (currentNumeral === key) {
+          newNums.push(DIGIT_VALUES[key])
+        }
+      }
+    }
+    console.log(newNums)
+
+    var runningTotal = 0;
+    for (var j = 0; j < newNums.length; j++) {
+      if (newNums[j] >= newNums[j + 1]) {
+        runningTotal += newNums[j] + newNums[j + 1];
+      }
+      if (newNums[j] < newNums[j + 1]) {
+        runningTotal += newNums[j + 1] - newNums[j];
+      }
+    }
+    return runningTotal;
+  }
+}
