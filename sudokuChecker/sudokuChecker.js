33eb4a223d31ff2af2178aedf62affa44035aead
Doug Perez <perezcpt@gmail.com>
2021-01-26 23:59:30 -0800

    sudoku

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -1,26 +1,40 @@
-/*
-Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' if the board is invalid.
+// /*
+// Given a full sudoku board, return 'solved' if the board is solved, or 'invalid' if the board is invalid.
 
-A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
+// A sudoku board is solved if each column, row, and 3 x 3 subgrid contains all of the digits from 1 to 9.
 
-Input: A String representing the board. 
-Output: 'solved' if the board is valid, 'invalid' if it isn't
+// Input: A String representing the board.
+// Output: 'solved' if the board is valid, 'invalid' if it isn't
 
-Example input: 
-"735814296\n
-896275314\n
-214963857\n
-589427163\n
-362189745\n
-471356982\n
-923541678\n
-648792531\n
-157638429"
-*/
+const input = "735814296896275314214963857589427163362189745471356982923541678648792531157638429";
+const input2 = "123456789123456789123456789123456789123456789123456789123456789123456789123456799";
+var chunks = input.match(/[\s\S]{1,9}/g) || [];
+
+
+
 
 
 
 function sudokuChecker(board) {
-  // Your code here.
-  
+  var chunks = board.match(/[\s\S]{1,9}/g) || [];
+  console.log(chunks)
+  for (var i = 0; i < chunks.length; i++) {
+    var dupesRemoved = new Set(chunks[i])
+    console.log(dupesRemoved.size)
+    if (dupesRemoved.size !== 9) {
+      return false;
+    }
+  }
+  return true
 }
+
+
+console.log(sudokuChecker(input))
+console.log(sudokuChecker(input2))
+
+
+
+//i: string of numbers
+//o: boolean, solved or invalid
+//c:
+//e:
