61ef1fe604a0890ef302cdf7e72cf4e1d5437b41
Doug Perez <perezcpt@gmail.com>
2021-05-20 21:18:59 -0700

    longest run compelte

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -16,18 +16,42 @@
 //TEST COMMIT
 
 var longestRun = function (string) {
-  // TODO: Your code here!
-};
+  var longest = [0, 0];
+  var currentRun = [0, 0];
 
-// If you need a random string generator, use this!
-// (you wont need this function for your solution but it may help with testing)
-var randomString = function (len) {
-  var text = "";
-  var possible = "abcdefghijklmnopqrstuvwxyz";
-
-  for (var i = 0; i < len; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (var i = 1; i < string.length; i++) {
+    if (string.charAt(i - 1) === string.charAt(i)) {
+      currentRun[1] = i;
+      if (currentRun[1] - currentRun[0] > longest[1] - longest[0]) {
+        longest = currentRun.slice();
+      }
+    } else {
+      currentRun = [i, i];
+    }
   }
-
-  return text;
+  return string.length > 0 ? longest : null;
 };
+
+//iterate over the string
+//
+
+// var randomString = function (len) {
+// var results = 0;
+// var currentRun = 0;
+
+//   var text = "";
+//   var possible = "abcdefghijklmnopqrstuvwxyz";
+
+//   for (var i = 0; i < len; i++) {
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//   }
+//   return text
+// }
+
+console.log(longestRun("abbbcc")); // [1, 3]
+console.log(longestRun("aabbc")); // [0, 1]
+console.log(longestRun("abcd")); // [0, 0]
+console.log(longestRun("")); // null
+console.log(longestRun(randomString(30))); //
+console.log(longestRun(randomString(20))); //
+console.log(longestRun(randomString(10))); //
